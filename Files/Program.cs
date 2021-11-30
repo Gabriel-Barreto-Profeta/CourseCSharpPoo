3b278ee208ea4e48dfa4679cd61a3234ad56903e
Gabriel Profeta <gabriel.profeta@terceiroslinx.com.br>
2021-11-30 19:45:01 -0300

    Implementando trabalho com arquivo e criando sempre um novo para não gerar duplicidade no processo.

---
+++ Files/Program.cs
@@ -0,0 +1,48 @@
+﻿using Files.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+
+namespace Files
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+            
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                CheckFile checkFile = new CheckFile(Path.GetDirectoryName(sourceFilePath));
+                var validaDirectory = Directory.Exists(checkFile.TargetFolderPath);
+
+                if (validaDirectory)
+                    checkFile.CheckExistingFile();
+                else
+                    Directory.CreateDirectory(checkFile.TargetFilePath);
+                
+                using (StreamWriter sw = File.AppendText(checkFile.TargetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
